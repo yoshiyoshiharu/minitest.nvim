253834456933228a18026726ac93859124617012
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-12 13:56:36 +0900

    style: fix lint

--- lua/runner.lua
+++ lua/runner.lua
@@ -3,8 +3,8 @@ local path = require('path')
 local M = {}
 
 function M.run()
-  local command = "rails test "  .. path.current_path()
-  print("Running " .. command)
+  local command = 'rails test' .. path.relative_path_from_git_root()
+  print('Running ' .. command)
 end
 
 return M
