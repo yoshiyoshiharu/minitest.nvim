3f9ec09037023d339091818ea3e67c0c5baa74bb
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-12 14:47:02 +0900

    feat: exec command in the split terminal

--- lua/runner.lua
+++ lua/runner.lua
@@ -4,8 +4,7 @@ local M = {}
 
 function M.run()
   local command = 'rails test ' .. path.relative_path_from_git_root()
-  print('Running ' .. command)
-  vim.api.nvim_command(command)
+  vim.api.nvim_command('split | terminal ' .. command)
 end
 
 return M
