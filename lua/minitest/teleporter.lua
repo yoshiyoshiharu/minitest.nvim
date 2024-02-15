04c57b879634cbbd62cfb7d269e36f543b037be5
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-15 23:27:53 +0900

    refactor: add guard clause to return not target file

--- lua/minitest/teleporter.lua
+++ lua/minitest/teleporter.lua
@@ -3,8 +3,14 @@ local M = {}
 local path = require('minitest.path')
 
 function M.teleport_to_test_file()
+  if not path.is_app_file(path.relative_path()) then
+    vim.notify('Not an app file: ' .. path.relative_path())
+    return
+  end
+
   local test_file = string.gsub(path.absolute_path(), 'app/', 'test/')
   test_file = string.gsub(test_file, '.rb', '_test.rb')
+  print(test_file)
 
   if not path.exists(test_file) then
     vim.notify('Test file does not exist: ' .. test_file)
@@ -15,6 +21,11 @@ function M.teleport_to_test_file()
 end
 
 function M.teleport_to_app_file()
+  if not path.is_test_file(path.relative_path()) then
+    vim.notify('Not a test file: ' .. path.relative_path())
+    return
+  end
+
   local app_file = string.gsub(path.absolute_path(), 'test/', 'app/')
   app_file = string.gsub(app_file, '_test.rb', '.rb')
 
@@ -25,5 +36,7 @@ function M.teleport_to_app_file()
 
   vim.cmd('find ' .. app_file)
 end
+
+M.teleport_to_test_file()
 
 return M
