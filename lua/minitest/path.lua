84fbcf473d045b98b5357a5fc4652dea8cc9b06f
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-15 21:47:50 +0900

    chore: add find files from app and test

--- lua/minitest/path.lua
+++ lua/minitest/path.lua
@@ -4,12 +4,26 @@ function M.absolute_path()
   return vim.api.nvim_buf_get_name(0)
 end
 
-function M.relative_path_from_git_root()
+function M.current_relative_path_from_git_root()
   local absolute_file_path = vim.fn.expand('%:p')
   local git_root = vim.fn.system('git rev-parse --show-toplevel')
   local relative_file_path = string.sub(absolute_file_path, string.len(git_root) + 1)
 
   return relative_file_path
+end
+
+function M.find_test_file_from_app_file(path)
+  local test_file = string.gsub(path, 'app/', 'test/')
+  test_file = string.gsub(test_file, '.rb', '_test.rb')
+
+  return test_file
+end
+
+function M.find_app_file_from_test_file(path)
+  local file = string.gsub(path, 'test/', 'app/')
+  file = string.gsub(file, '_test.rb', '.rb')
+
+  return file
 end
 
 return M
