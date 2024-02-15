d3b146637dad042176f8b0873db953802637ea3c
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-15 21:32:40 +0900

    fix: fix relative_file_path

--- lua/minitest/path.lua
+++ lua/minitest/path.lua
@@ -7,7 +7,7 @@ end
 function M.relative_path_from_git_root()
   local absolute_file_path = vim.fn.expand('%:p')
   local git_root = vim.fn.system('git rev-parse --show-toplevel')
-  local relative_file_path = string.sub(absolute_file_path, string.len(git_root) + 2)
+  local relative_file_path = string.sub(absolute_file_path, string.len(git_root) + 1)
 
   return relative_file_path
 end
