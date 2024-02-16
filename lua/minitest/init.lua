f99163055dc12c21711920ffec808c29eb6ffa7b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-16 19:18:54 +0900

    feat: add user command

--- lua/minitest/init.lua
+++ lua/minitest/init.lua
@@ -5,10 +5,10 @@ local config = require('minitest.config')
 
 function M.setup(user_config)
   config.setup(user_config)
+  vim.api.nvim_command "command! MintestRun lua require('minitest.runner').run()<CR>"
+  vim.api.nvim_command "command! MinitestTeleportToApp lua require('minitest.teleporter').teleport_to_app_file()<CR>"
+  vim.api.nvim_command "command! MinitestTeleportToTest lua require('minitest.teleporter').teleport_to_test_file()<CR>"
 end
 
-function M.exec()
-  runner.run()
-end
 
 return M
