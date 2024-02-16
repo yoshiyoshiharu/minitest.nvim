8e13a6cf94e4b685e7c0d6956aa1af1fa313a7ef
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-16 19:27:48 +0900

    feat: unify teleport to app and test

--- lua/minitest/init.lua
+++ lua/minitest/init.lua
@@ -1,14 +1,11 @@
 local M = {}
 
-local runner = require('minitest.runner')
 local config = require('minitest.config')
 
 function M.setup(user_config)
   config.setup(user_config)
-  vim.api.nvim_command "command! MintestRun lua require('minitest.runner').run()<CR>"
-  vim.api.nvim_command "command! MinitestTeleportToApp lua require('minitest.teleporter').teleport_to_app_file()<CR>"
-  vim.api.nvim_command "command! MinitestTeleportToTest lua require('minitest.teleporter').teleport_to_test_file()<CR>"
+  vim.api.nvim_command 'command! MintestRun lua require('minitest.runner').run()<CR>'
+  vim.api.nvim_command 'command! MinitestTeleport lua require('minitest.teleporter').teleport()<CR>'
 end
-
 
 return M
--- lua/minitest/teleporter.lua
+++ lua/minitest/teleporter.lua
@@ -2,38 +2,38 @@ local M = {}
 
 local path = require('minitest.path')
 
-function M.teleport_to_test_file()
-  if not path.is_app_file(path.relative_path()) then
-    vim.notify('Current buffer is not an app file: ' .. path.relative_path())
-    return
+function M.teleport()
+  local function teleport_to_app_file()
+    local app_file = string.gsub(path.absolute_path(), 'test/', 'app/')
+    app_file = string.gsub(app_file, '_test.rb', '.rb')
+
+    if not path.exists(app_file) then
+      vim.notify('App file does not exist: ' .. app_file)
+      return
+    end
+
+    vim.cmd('find ' .. app_file)
   end
 
-  local test_file = string.gsub(path.absolute_path(), 'app/', 'test/')
-  test_file = string.gsub(test_file, '.rb', '_test.rb')
+  local function teleport_to_test_file()
+    local test_file = string.gsub(path.absolute_path(), 'app/', 'test/')
+    test_file = string.gsub(test_file, '.rb', '_test.rb')
 
-  if not path.exists(test_file) then
-    vim.notify('Test file does not exist: ' .. test_file)
-    return
+    if not path.exists(test_file) then
+      vim.notify('Test file does not exist: ' .. test_file)
+      return
+    end
+
+    vim.cmd('find ' .. test_file)
   end
 
-  vim.cmd('find ' .. test_file)
-end
-
-function M.teleport_to_app_file()
-  if not path.is_test_file(path.relative_path()) then
-    vim.notify('Current buffer is not a test file: ' .. path.relative_path())
-    return
+  if path.is_test_file(path.relative_path()) then
+    teleport_to_app_file()
+  elseif path.is_app_file(path.relative_path()) then
+    teleport_to_test_file()
+  else
+    vim.notify('Current buffer is not a app or test file: ' .. path.relative_path())
   end
-
-  local app_file = string.gsub(path.absolute_path(), 'test/', 'app/')
-  app_file = string.gsub(app_file, '_test.rb', '.rb')
-
-  if not path.exists(app_file) then
-    vim.notify('App file does not exist: ' .. app_file)
-    return
-  end
-
-  vim.cmd('find ' .. app_file)
 end
 
 return M
