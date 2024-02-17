27643215919a20d77a99ff46ca0ec739f06f76fc
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-17 11:37:18 +0900

    fix: nvim command

--- lua/minitest/init.lua
+++ lua/minitest/init.lua
@@ -4,8 +4,8 @@ local config = require('minitest.config')
 
 function M.setup(user_config)
   config.setup(user_config)
-  vim.api.nvim_command 'command! MintestRun lua require('minitest.runner').run()<CR>'
-  vim.api.nvim_command 'command! MinitestTeleport lua require('minitest.teleporter').teleport()<CR>'
+  vim.api.nvim_command "command! MintestRun lua require('minitest.runner').run()<CR>"
+  vim.api.nvim_command "command! MinitestTeleport lua require('minitest.teleporter').teleport()<CR>"
 end
 
 return M
