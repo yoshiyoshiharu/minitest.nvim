d08ca107c87d6beca16bc946b584593383a7ce6a
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-17 10:59:34 +0900

    feat: add create file when teleport target file is not exist

--- lua/minitest/teleporter.lua
+++ lua/minitest/teleporter.lua
@@ -3,13 +3,25 @@ local M = {}
 local path = require('minitest.path')
 
 function M.teleport()
+  local function create_file(path, app_or_test)
+    vim.ui.input({ prompt = 'Create ' .. app_or_test .. 'file? y/N: ' }, function(input)
+      if input == 'y' then
+        path.create_file(path)
+        return true
+      else
+        return false
+      end
+    end)
+  end
+
   local function teleport_to_app_file()
     local app_file = string.gsub(path.absolute_path(), 'test/', 'app/')
     app_file = string.gsub(app_file, '_test.rb', '.rb')
 
     if not path.exists(app_file) then
-      vim.notify('App file does not exist: ' .. app_file)
-      return
+      if not create_file(app_file, 'app') then
+        return
+      end
     end
 
     path.goto_file(app_file)
@@ -20,8 +32,9 @@ function M.teleport()
     test_file = string.gsub(test_file, '.rb', '_test.rb')
 
     if not path.exists(test_file) then
-      vim.notify('Test file does not exist: ' .. test_file)
-      return
+      if not create_file(test_file, 'test') then
+        return
+      end
     end
 
     path.goto_file(test_file)
