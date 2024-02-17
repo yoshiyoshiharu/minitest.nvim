0133490eefbaeebe08d0236d61f021cb714d4ca2
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-17 11:58:22 +0900

    fix: typo command

--- lua/minitest/init.lua
+++ lua/minitest/init.lua
@@ -4,7 +4,7 @@ local config = require('minitest.config')
 
 function M.setup(user_config)
   config.setup(user_config)
-  vim.api.nvim_command "command! MintestRun lua require('minitest.runner').run()<CR>"
+  vim.api.nvim_command "command! MinitestRun lua require('minitest.runner').run()<CR>"
   vim.api.nvim_command "command! MinitestTeleport lua require('minitest.teleporter').teleport()<CR>"
 end
 
