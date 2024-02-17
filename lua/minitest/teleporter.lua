d5efd3d4e436259f84563c6e40d83b3147a72899
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-17 11:46:28 +0900

    refactor: create_file

--- lua/minitest/teleporter.lua
+++ lua/minitest/teleporter.lua
@@ -3,13 +3,11 @@ local M = {}
 local pathlib = require('minitest.path')
 
 function M.teleport()
-  local function create_file(path, app_or_test)
+  local function create_and_goto_file(path, app_or_test)
     vim.ui.input({ prompt = 'Create ' .. app_or_test .. 'file? y/N: ' }, function(input)
       if input == 'y' then
         pathlib.create_file(path)
-        return true
-      else
-        return false
+        pathlib.goto_file(path)
       end
     end)
   end
@@ -18,26 +16,22 @@ function M.teleport()
     local app_file = string.gsub(pathlib.absolute_path(), 'test/', 'app/')
     app_file = string.gsub(app_file, '_test.rb', '.rb')
 
-    if not pathlib.exists(app_file) then
-      if not create_file(app_file, 'app') then
-        return
-      end
+    if pathlib.exists(app_file) then
+      pathlib.goto_file(app_file)
+    else
+      create_and_goto_file(app_file, 'app')
     end
-
-    pathlib.goto_file(app_file)
   end
 
   local function teleport_to_test_file()
     local test_file = string.gsub(pathlib.absolute_path(), 'app/', 'test/')
     test_file = string.gsub(test_file, '.rb', '_test.rb')
 
-    if not pathlib.exists(test_file) then
-      if not create_file(test_file, 'test') then
-        return
-      end
+    if pathlib.exists(test_file) then
+      pathlib.goto_file(test_file)
+    else
+      create_and_goto_file(test_file, 'test')
     end
-
-    pathlib.goto_file(test_file)
   end
 
   if pathlib.is_test_file(pathlib.relative_path()) then
