a32942c0a8f4016ef298a08669d81d7c05c83130
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-02-18 18:09:03 +0900

    fix: do not execute if current buffer is not a test file

--- lua/minitest/runner.lua
+++ lua/minitest/runner.lua
@@ -1,10 +1,15 @@
-local path = require("minitest.path")
+local pathlib = require("minitest.path")
 local config = require("minitest.config")
 
 local M = {}
 
 function M.run()
-  local command = config.get("command") .. " " .. path.relative_path()
+  if not pathlib.is_test_file(pathlib.relative_path()) then
+    vim.notify "Current buffer is not a test file."
+    return
+  end
+
+  local command = config.get("command") .. " " .. pathlib.relative_path()
 
   local bufnr = vim.api.nvim_create_buf(false, true)
 
